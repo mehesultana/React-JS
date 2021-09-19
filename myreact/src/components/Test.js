cdb052ece3df36ad5554350710670a6870f0ed18
mehesultana <sultanamehe01@gmail.com>
2021-09-19 21:17:25 +0600

    React State & Lifecycle in Class Component

---
+++ myreact/src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h2>hello test</h2>
+            <p>meheeee</p>
+        </div>
+    );
+};
+
+export default Test;
